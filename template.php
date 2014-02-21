8eae38fe59a61cc3501f6e04fd604fbdc4cbc992
Worapat <doo_doo_49@hotmail.com>
2014-02-21 09:31:20 +0700

    Add template.php

---
+++ template.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<title>GMMZ</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link rel="stylesheet" type="text/css" href="css/layout.css" media="all" />
+<script type="text/javascript" src="js/jquery.js"></script>  
+<script type="text/javascript" src="js/shared.js"></script>  
+</head>
+<body id="g1">
+<div id="wrapper">
+
+  <?php include('inc/inc-header.php'); ?>
+
+	<div id="contents">
+		Enter here...        
+    <!-- /contents --></div>
+<!-- /wrapper --></div>
+
+<?php include('inc/inc-footer.php'); ?>
+
+</body>
+</html>
