3ebded836f5b4eec3784eec37bc52934d1732b01
Worapat <doo_doo_49@hotmail.com>
2014-02-28 16:10:19 +0700

    Add more

---
+++ program.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<title>GMMZ</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link rel="stylesheet" type="text/css" href="css/layout.css" media="all" />
+<link rel="stylesheet" type="text/css" href="css/program.css" media="all" />
+ <script type="text/javascript" src="js/jquery.js"></script>   
+<script type="text/javascript" src="js/shared.js"></script> 
+</head>
+<body id="g2">
+<div id="wrapper">
+
+  <?php include('inc/inc-header.php'); ?>
+
+	<div id="contents">
+		<div class="section">
+        	<div id="series">
+            	<img src="images/pic_program01.jpg" width="978" height="345" alt="" /> 
+            <!-- / #series --></div>
+        <!-- /section --></div>
+        <div class="section">
+       	  <div id="calendarWrap" class="clearfix">
+             	<p class="heading">มีนาคม2557</p>
+                <div id="calendarIn" class="clearfix">
+                	<div id="calendar" class="clearfix">
+                		<ul>
+                			<li><a href="#">&nbsp;</a></li>
+                			<li><a href="#">&nbsp;</a></li>
+                			<li><a href="#">&nbsp;</a></li>
+                			<li><a href="#">&nbsp;</a></li>
+                			<li><a href="#">&nbsp;</a></li>
+                			<li><a href="#">&nbsp;</a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal01.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal02.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal03.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal04.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal05.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal06.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal07.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal08.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal09.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal10.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal11.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal12.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal13.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal14.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal15.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal16.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal17.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal18.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal19.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal20.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal21.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal22.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal23.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal24.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#"><img src="images/pic_thumbCal25.gif" width="137" height="118" alt="" /></a></li>
+                			<li><a href="#">&nbsp;</a></li>
+                			<li><a href="#">&nbsp;</a></li>
+                			<li><a href="#">&nbsp;</a></li>
+                			<li><a href="#">&nbsp;</a></li>
+                		</ul>
+                	<!-- / #calendar --></div>
+             <!-- / #calendarIn --></div>
+          <!-- / #calendarWrap --></div>
+        <!-- /section --></div>
+
+		<div id="varietyWrap" class="section">
+          	<div id="variety">
+        		<p class="heading">วาไรตี้บันเทิง/Variety</p>
+            	<div id="slidesVariety" class="slides">
+                  <img src="images/pic_varietySlide01.jpg" width="695" height="330" alt="" />           		  
+                  <img src="images/pic_varietySlide01.jpg" width="695" height="330" alt="" />           		  
+                  <img src="images/pic_varietySlide01.jpg" width="695" height="330" alt="" />           		  
+                </div>
+              	<div class="category">
+                	<ul id="tabs">
+                		<li><a id="tab1" href="#">ภาพยนตร์/Movies </a></li>
+                		<li><a id="tab2" href="#">ซีรีส์/Series</a></li>
+                		<li><a id="tab3" href="#">กีฬา/Sport </a></li>
+                		<li><a id="tab4" href="#">สารคดี/Documentary</a></li>
+                		<li><a id="tab5" href="#">ครอบครัว/Family </a></li>
+                		<li><a id="tab6" href="#">วาไรตี้บันเทิง/Variety</a></li>
+                		<li><a id="tab7" href="#">เพลง/Music</a></li>
+                		<li><a id="tab8" href="#">ข่าว / News</a></li>
+                	</ul>
+                </div>
+            <!-- / #variety --></div>
+        <!-- /section --></div>
+        
+  <!-- /contents --></div>
+<!-- /wrapper --></div>
+
+<?php include('inc/inc-footer.php'); ?>
+
+<script type="text/javascript" src="js/jquery.slides.min.js"></script>
+<script type="text/javascript">	
+	$(document).ready(function(){
+		//top slides
+      $('#slidesVariety').slidesjs({
+        width: 695,
+        height: 330
+      });
+	  
+	  //group slider
+	  $('#tabs li a').click(function(){
+		  var t = $(this).attr('id');
+		  if($(this).hasClass('inactive')){ //this is the start of our condition 
+			$('#tabs li a').addClass('inactive');           
+			$(this).removeClass('inactive');
+		
+			$('.slides').hide();
+			$('#'+ t + 'C').fadeIn('slow');
+		 }
+		});
+	});
+</script> 
+
+</body>
+</html>
